60ead8f40a50ed51b8cba7798383eb7afce3494c
kaustubh mane <kaustubh6220@gmail.com>
2024-03-25 18:37:13 +0530

    clerk webhook

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -20,7 +20,7 @@ export const connectToDatabase= async()=>{
 
     if(!MONGODB_URL) throw new Error('MONGODB_URL is not defined');
 
-    cached.promise = cached.promise || mongoose.connect(MONGODB_URL,{dbName:'LearnWise',bufferCommands:false});
+    cached.promise = cached.promise || mongoose.connect(MONGODB_URL,{dbName:'learnwise',bufferCommands:false});
 
     cached.conn = await cached.promise;
 
--- types/index.t.ts
+++ types/index.t.ts
@@ -1,21 +1,23 @@
 /* eslint-disable no-unused-vars */
 
 // ====== USER PARAMS
+// ====== USER PARAMS
 declare type CreateUserParams = {
-    clerkId: string;
-    email: string;
-    username: string;
-    firstName: string;
-    lastName: string;
-    photo: string;
-  };
-  
-  declare type UpdateUserParams = {
-    firstName: string;
-    lastName: string;
-    username: string;
-    photo: string;
-  };
+  clerkId: string;
+  email: string;
+  username: string;
+  firstName: string;
+  lastName: string;
+  photo: string;
+};
+
+declare type UpdateUserParams = {
+  firstName: string;
+  lastName: string;
+  username: string;
+  photo: string;
+};
+
   
   // ====== IMAGE PARAMS
   declare type AddImageParams = {
