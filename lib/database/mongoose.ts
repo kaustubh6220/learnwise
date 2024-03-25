5703049ebbd14bbe1971c72030e2ff1097d03374
kaustubh mane <kaustubh6220@gmail.com>
2024-03-26 03:35:35 +0530

    implement clerk

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -1,30 +1,18 @@
-import mongoose, { Mongoose } from 'mongoose';
+import mongoose from 'mongoose';
 
-const MONGODB_URL = process.env.MONGODB_URL;
+const MONGODB_URI = process.env.MONGODB_URI;
 
-interface MongooseConnection {
-  conn: Mongoose | null;
-  promise: Promise<Mongoose> | null;
-}
-
-let cached: MongooseConnection = (global as any).mongoose
-
-if(!cached) {
-  cached = (global as any).mongoose = { 
-    conn: null, promise: null 
-  }
-}
+let cached = (global as any).mongoose || { conn: null, promise: null };
 
 export const connectToDatabase = async () => {
-  if(cached.conn) return cached.conn;
+  if (cached.conn) return cached.conn;
 
-  if(!MONGODB_URL) throw new Error('Missing MONGODB_URL');
+  if(!MONGODB_URI) throw new Error('MONGODB_URI is missing');
 
-  cached.promise = 
-    cached.promise || 
-    mongoose.connect(MONGODB_URL, { 
-      dbName: 'learnwise', bufferCommands: false 
-    })
+  cached.promise = cached.promise || mongoose.connect(MONGODB_URI, {
+    dbName: 'learnwise',
+    bufferCommands: false,
+  })
 
   cached.conn = await cached.promise;
 
